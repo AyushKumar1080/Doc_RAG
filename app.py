8b24b7cd02403a57e1d2a07834663c1134382da6
AyushK1080 <kiran77high@gmail.com>
2026-07-12 19:12:28 +0530

    Recent commit

--- app.py
+++ app.py
@@ -1,61 +1,103 @@
-import tempfile
+import os
+import shutil
 
-import streamlit as st
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
 
 from embeddings_file import build_vector_store
 from prompt_model import chain
 
-st.title("Hybrid RAG")
+app = FastAPI()
 
-uploaded_file = st.file_uploader(
-    "Upload Document",
-    type=["pdf","txt","csv","docx"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-if uploaded_file:
+vector_store = None
 
-    with tempfile.NamedTemporaryFile(
-        delete=False,
-        suffix="."+uploaded_file.name.split(".")[-1]
-    ) as tmp:
 
-        tmp.write(uploaded_file.read())
+@app.post("/upload")
 
-        file_path = tmp.name
+async def upload_document(file: UploadFile = File(...)):
+
+    global vector_store
+
+    os.makedirs("uploads", exist_ok=True)
+
+    file_path = os.path.join(
+        "uploads",
+        file.filename
+    )
+
+    with open(file_path, "wb") as f:
+
+        shutil.copyfileobj(file.file, f)
 
     vector_store = build_vector_store(file_path)
 
+    return {
+        "message": "Document Uploaded Successfully"
+    }
+
+
+@app.post("/ask")
+
+async def ask_question(question: str = Form(...)):
+
+    global vector_store
+
+    if vector_store is None:
+
+        return {
+            "answer":"Please upload a document first."
+        }
+
     retriever = vector_store.as_retriever(
+
         search_type="mmr",
+
         search_kwargs={
+
             "k":4,
             "fetch_k":16,
             "lambda_mult":0.5
+
         }
+
     )
 
-    query = st.text_input("Ask a question")
+    docs = retriever.invoke(question)
 
-    if st.button("Submit"):
+    context = "\n\n".join(
 
-        docs = retriever.invoke(query)
+        doc.page_content
 
-        context = "\n\n".join(
+        for doc in docs
+
+    )
+
+    answer = chain.invoke({
+
+        "context":context,
+
+        "question":question
+
+    })
+
+    return {
+
+        "answer":answer,
+
+        "chunks":[
+
             doc.page_content
+
             for doc in docs
-        )
 
-        answer = chain.invoke({
-            "context": context,
-            "question": query
-        })
+        ]
 
-        st.subheader("Answer")
-
-        st.write(answer)
-
-        with st.expander("Retrieved Chunks"):
-
-            for doc in docs:
-                st.write(doc.page_content)
-                st.divider()
+    }
